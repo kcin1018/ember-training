aa35b521fa3442e44495c0fcdfb30b9bfb1a4375
Nick Felicelli <nicholas.felicelli@cchmc.org>
2016-02-18 11:45:32 -0500

    Completing exercise-2b

--- app/routes/org/repos.js
+++ app/routes/org/repos.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return [
+      { id: 'data', text: 'data' },
+      { id: 'website', text: 'website' },
+      { id: 'guides', text: 'guides' },
+      { id: 'ember.js', text: 'ember.js' },
+      { id: 'rfcs', text: 'rfcs' },
+    ];
+  }
 });
