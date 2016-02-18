e71ee00fd5e9bc72548736503d354128a63952e4
Nick Felicelli <nicholas.felicelli@cchmc.org>
2016-02-18 12:23:54 -0500

    Finished implementation for exercise-3

--- app/routes/orgs.js
+++ app/routes/orgs.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  favorites: Ember.inject.service(),
   model() {
     return [
       { id: 'facebook', text: 'Facebook' },
@@ -8,5 +9,10 @@ export default Ember.Route.extend({
       { id: 'yahoo', text: 'Yahoo' },
       { id: 'ember.js', text: 'Ember.js' },
     ];
+  },
+  actions: {
+    favorite(org) {
+      this.get('favorites').favorite(org);
+    }
   }
 });
