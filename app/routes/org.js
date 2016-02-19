16445cba75fed2d3b6efdbd2ef6b472ae9f56fcc
Nick Felicelli <nicholas.felicelli@cchmc.org>
2016-02-18 21:10:03 -0500

    Fixing some things

--- app/routes/org.js
+++ app/routes/org.js
@@ -11,7 +11,11 @@ export default Ember.Route.extend({
   actions: {
     error(jqXhr, transition, route) {
       if(jqXhr.status === 404) {
-        this.intermediateTransitionTo('org.not-found');
+        // if 404 then handle it differently
+        this.intermediateTransitionTo('org.notfound');
+      } else {
+        // bubble up to the default error state
+        return true;
       }
     }
   }
