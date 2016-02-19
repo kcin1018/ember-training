366ce5573540d6224dfdf98d5462d5c52032e296
Nick Felicelli <nicholas.felicelli@cchmc.org>
2016-02-19 11:39:56 -0500

    Finishing exercise #7

--- app/services/favorites.js
+++ app/services/favorites.js
@@ -3,7 +3,11 @@ import Ember from 'ember';
 export default Ember.Service.extend({
   items: [],
   favorite(org) {
-    this.get('items').addObject(org);
+    if(this.get('items').indexOf(org) === -1) {
+      this.get('items').addObject(org);
+    } else {
+      this.get('items').popObject(org);
+    }
     console.log(this.get('items').join(','));
   }
 });
